f91ba82edfe9bacf222d0089cca3421dd6c0c031
Thiago Anjos <tafas.anjos@gmail.com>
2023-06-17 16:46:38 -0300

    #Aula4 Criacao do modulo siebar e passagem de parametros entre os modulos

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Thiago"
+          description="Brecker o goleiro"
+          music="Musica épica de fundo"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video
+          likes={444}
+          messages={555}
+          shares={666}
+          name="Paulo"
+          description="Bird olhando para a camera"
+          music="Clap your hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+        />
       </div>
     </div>
   );
