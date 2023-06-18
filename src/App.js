be2704636c18bb2c3bf25b888f3b7cb206c3c825
Thiago Anjos <tafas.anjos@gmail.com>
2023-06-18 18:26:49 -0300

    #Aula5 Criacao e integracao com o Firebase

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Video from './pages/Video'
+import db from './config/firebase'
+import { collection, getDocs } from 'firebase/firestore/lite'
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosColletion = collection(db, 'videos');
+    const videosSnapshot = await getDocs(videosColletion);
+    const videoList = videosSnapshot.docs.map(doc => doc.data());
+    setVideos(videoList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, [])
+
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video
-          likes={111}
-          messages={222}
-          shares={333}
-          name="Thiago"
-          description="Brecker o goleiro"
-          music="Musica épica de fundo"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-        />
-        <Video
-          likes={444}
-          messages={555}
-          shares={666}
-          name="Paulo"
-          description="Bird olhando para a camera"
-          music="Clap your hands"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
-        />
+
+        {
+          videos.map((item) => {
+            return (
+              <Video
+                likes={item.likes}
+                messages={item.messages}
+                shares={item.shares}
+                name={item.name}
+                description={item.description}
+                music={item.music}
+                url={item.url}
+              />
+            )
+          })
+        }
       </div>
     </div>
   );
